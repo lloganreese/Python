6cabce4e18fd62201b22feb32d85f000ccb6a91e
Logan <lloganreese@gmail.com>
2020-07-13 20:27:25 -0400

    -created stock ticker information finder

--- Stock_Information/main.py
+++ Stock_Information/main.py
@@ -1,7 +1,12 @@
 import sys
+import yfinance as yf
 
-stocksymbol = sys.argv
-stocksymbol.pop(0)
+stockSymbol = input("Enter your stock: ")
 
-print(stocksymbol)
+tickerData = yf.Ticker(stockSymbol)
+
+tickerDf = tickerData.history(period='1d', start='2020-1-1', end='2020-1-24')
+
+print(tickerDf)
+
 
